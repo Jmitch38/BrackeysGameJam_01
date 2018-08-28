be26da9712efa917315eda87ef2fa2bdaf7e6288
Jmitch38 <14fansemail@gmail.com>
2018-08-28 18:39:56 -0400

    PlayerController is done, working on main menu

--- LightField/Assets/Scripts/PlayerController.cs
+++ LightField/Assets/Scripts/PlayerController.cs
@@ -1,45 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-    /*---Script Hieracry---
+    /*---Script order---
      * Varables
-     * --Start--
+     * --Start function--
      * --Update Funtion--
      * -Movement-
      * -Turn-
      * -Fire-
      * -Laser-
+     * -UI-
      * --Fixed Update Function--
-     */ 
+     * --On trigger enter function--
+     */
+
+    //Variables and other
+    #region
     int LaserSelction;
     float FireTimer;
-	[Header("--Ship Variables--")]
+    GameObject CurrentLaser;
+
+    [Header("--Ship Variables--")]
     public float Speed;
     public int health;
+
     [Header("--Lasers and Gun--")]
-    GameObject CurrentLaser;
     public GameObject BlueLaser;
     public GameObject YellowLaser;
     public GameObject RedLaser;
     public Transform LaserSpawn1;
     public Transform LaserSpawn2;
 
+    [Header("--UI--")]
+    public Text PlayerHealth;
+    public Text YouDied;
+    public Text CurrentLaserSelection;
+    #endregion
+
     void Start()
     {
+        #region
+        YouDied.gameObject.SetActive(false);
         FireTimer = 0;
         LaserSelction = 0;
+        #endregion
     }
 
     void Update ()
     {
-        Movement(); //how the player moves
-        Turn(); //how the player turns
-        Fire(); //how the player shoots
-        Laser(); //How the player slects laser
-	}
+        #region
+        if(health > 0)
+        {
+            Movement(); //how the player moves
+            Turn(); //how the player turns
+            Fire(); //how the player shoots
+            Laser(); //How the player slects laser
+        }      
+        UI(); //UI for playership
+        LoadScene(); //loading the start scene when the player dies
+        #endregion
+    }
 
     void Movement()
     {
@@ -56,6 +81,7 @@ public class PlayerController : MonoBehaviour
         {
             transform.Translate(0, 0, 0, Space.World);
         }
+
         if (Input.GetAxis("LeftRight") > 0)
         {
             transform.Translate(Speed * Time.deltaTime, 0, 0, Space.World);
@@ -76,18 +102,19 @@ public class PlayerController : MonoBehaviour
         #region
         if (Input.GetAxis("LeftClick") > 0)
         {
-            Vector3 upAxis = new Vector3(0, 0, -1);
+            Vector3 upAxis = new Vector3(0, 0, 1);
             Vector3 mouseScreenPosition = Input.mousePosition;
             mouseScreenPosition.z = transform.position.z;
             Vector3 mouseWorldSpace = Camera.main.ScreenToWorldPoint(mouseScreenPosition);
             transform.LookAt(mouseWorldSpace, upAxis);
-            transform.eulerAngles = new Vector3(0, 0, -transform.eulerAngles.z + 180);
+            transform.eulerAngles = new Vector3(0, 0, -transform.eulerAngles.z);
         }
         #endregion
     }
 
     void Fire()
     {
+        #region
         if(Input.GetAxis("LeftClick") > 0)
         {
             if(FireTimer <= 0)
@@ -97,6 +124,7 @@ public class PlayerController : MonoBehaviour
                 FireTimer += 2;
             }
         }
+        #endregion
     }
 
     void Laser()
@@ -126,12 +154,69 @@ public class PlayerController : MonoBehaviour
         #endregion
     }
 
+    void UI()
+    {
+        #region
+        PlayerHealth.text = "Health: " + health;  
+        
+        if(LaserSelction == 0)
+        {
+            CurrentLaserSelection.text = "Laser: Blue";
+        }
+        else if (LaserSelction == 1)
+        {
+            CurrentLaserSelection.text = "Laser: Yellow";
+        }
+        else if (LaserSelction == 2)
+        {
+            CurrentLaserSelection.text = "Laser: Red";
+        }
+
+        if (health <= 0)
+        {
+            YouDied.gameObject.SetActive(true);
+        }
+        #endregion
+    }
+
+    void LoadScene()
+    {
+        #region
+        if(health <= 0)
+        {
+            if(Input.GetButtonDown("ESC"))
+            {
+                SceneManager.LoadSceneAsync("MainMenu");
+            }
+        }
+        #endregion
+    }
+
     void FixedUpdate()
     {
-        print(FireTimer);
+        #region
         if(FireTimer > 0)
         {
             FireTimer -= 1 * Time.fixedDeltaTime;
         }
+        #endregion
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        #region
+        if (other.gameObject.tag == "YellowEnemy")
+        {
+            health -= 1;
+        }
+        if(other.gameObject.tag == "BlueEnemy")
+        {
+            health -= 1;
+        }
+        if(other.gameObject.tag == "RedEnemy")
+        {
+            health -= 1;
+        }
+        #endregion
     }
 }
